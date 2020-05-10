268726fabe11931039573a45b53988f5703a0066
aydingltp <aydngltp@gmail.com>
2020-05-10 08:00:18 +0300

    router link added

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,19 +1,28 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #563F7A">
-        <a class="navbar-brand" href="#">Ürün Listesi</a>
+
+        <router-link
+                tag = "a"
+                to = "/"
+                class = "navbar-brand">
+            Ürün Listesi
+        </router-link>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <router-link
-                active-class = "active"
-                tag = "li"
-                to = "/urun-islemleri"
-                class = "nav-item"
-                >
-                    <a class="nav-link" href="#">Ürün İşlemleri</a>
+                    active-class = "active"
+                    tag = "li"
+                    to = "/urun-islemleri"
+                    class = "nav-item">
+                <a class="nav-link" href="#">Ürün İşlemleri</a>
                 </router-link>
-                <li class="nav-item">
+                <router-link
+                        active-class = "active"
+                        tag = "li"
+                        to = "/urun-cikisi"
+                        class = "nav-item">
                     <a class="nav-link" href="#">Ürün Çıkışı</a>
-                </li>
+                </router-link>
             </ul>
         </div>
     </nav>
