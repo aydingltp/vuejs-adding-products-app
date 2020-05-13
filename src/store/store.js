3cf069634e8b2f10a5e75d2526c1ad2b32371bf0
aydingltp <aydngltp@gmail.com>
2020-05-13 07:58:27 +0300

    vue resource added

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import product from "./modules/product";
+import * as getters from "./getters";
+import * as mutations from "./mutations";
+import * as actions from "./actions";
 
 Vue.use(Vuex);
 export const store = new Vuex.Store({
@@ -10,6 +13,9 @@ export const store = new Vuex.Store({
         balance : 0.0,
 
     },
+    getters,
+    mutations,
+    actions,
     modules : {
         product
     }
